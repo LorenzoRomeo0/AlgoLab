8c7d8ad585927ead63d19df3cd2627295e7d50d3
LorenzoRomeo0 <lorenzo.romeo.mail@gmail.com>
2020-12-21 15:53:22 +0000

    Completamento "registro"

--- L5/registro.c
+++ L5/registro.c
@@ -4,20 +4,28 @@
 
 typedef char* string;
 
+
 int size = 0;
 
+/*  Restituisce una nuova lista di dimensione N.
+    Inizializza le posizioni NULL.
+    Stampa un errore se l'allocazione non ha successo.
+    EFFETTI-COLL:   Imposta size = n.
+*/
 string* newBook(int n){
-    string* book = malloc(sizeof(string*)*n);
+    string* book = malloc(sizeof(string)*n);
     if(book == NULL){
         printf("Errore drante la creazione della struttura.");
         return NULL;
     }
     size = n;
-    for(int i=0; i<n;i++) book[i] = malloc(sizeof(string*)*n);
     for(int i=0; i<n;i++) book[i] = NULL;
     return book;
 }
 
+/*  Clona la stringa "name" nella posizione k della lista book.
+    Stampa un errore se la posizione e' occupata oppure se non e' valida.
+*/
 void book(string* book, int k, string name){
     if(k<0 || k>=size){
         printf("Posizione non valida.\n");
@@ -27,21 +35,25 @@ void book(string* book, int k, string name){
         printf("Posizione gia' occupata.\n");
         return;
     }
-    book[k] = name;
+    int i = 0;
+    for(; name[i]!='\0';i++){}
+    book[k]=malloc(sizeof(char)*i);
+    strcpy(book[k], name);
 }
 
+/*  Cancella la prenotazione in posizione k impostandola a NULL.
+    Stampa un errore se la posizione non e' valida.
+*/
 void cancel(string* book, int k){
     if(k<0 || k>=size){
         printf("Posizione non valida.\n");
         return;
     }
-    if(book[k]!=NULL){
-        printf("Posizione gia' occupata.\n");
-        return;
-    }
     book[k]=NULL;
 }
 
+/*Sposta il valore in posizione from in to.
+Stampa un errore se from e' vuota o to e' occupata. */
 void move(string* book, int from, int to){
     if(from<0 || from>=size || to<0 || to>=size){
         printf("Posizione non valida.\n");
@@ -55,6 +67,7 @@ void move(string* book, int from, int to){
     book[from]=NULL;
 }
 
+/*Stampa il book. I posti con valore NULL vengono stampati come '.' */
 void printBook(string* book){
     for(int i=0; i<size; i++){
         if(book[i]==NULL)
@@ -64,30 +77,7 @@ void printBook(string* book){
     }
 }
 
-/*void commandParser(string* command){
-    printf("parser\n");
-    static string atom = " ";
-    int cont = 0;
-    do{
-        printf("----%s----%d\n", atom, cont);
-        if(strcmp(atom, " ") > 0){
-            printf("dsd\n");
-            command[cont] = malloc(sizeof(string)*3);
-            command[cont] = atom;
-            cont++;
-            printf("%s", command[cont]);
-        }
-        scanf("%s", atom);
-    }while(atom[0]!='\n' || cont<3);
-}*/
-void commandParser(){
-    string command;
-    string parsed;
-    for(int i=0; command[i]!='\0'; i++){
-
-    }
-}
-
+/*Inutilizzata */
 void split(string* ris, string s){
     int spazi = 0;
     string comodo;
@@ -144,11 +134,6 @@ int main(){
                 break;
 
         }
-}
-
-    
-
-    
-
+    }
     return 0;
 }
